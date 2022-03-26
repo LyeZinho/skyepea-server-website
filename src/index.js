4346afb3cb6254685db3874290dafc87cd584e4e
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-03-26 15:22:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3000;
 //Rota para o sub dominio esp [Sub dominio principal]
 
 //Paginas
-const skyrouter = require('./routes_esp/router.js')
+const skyrouter = require('.router.js')
 app.use(skyrouter);
 //Dados 
-const skyrouterdat = require('./routes_esp/datarouter.js')
+const skyrouterdat = require('.datarouter.js')
 app.use(skyrouterdat);
 
 
