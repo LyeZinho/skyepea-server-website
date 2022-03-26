a2c629500801c097ef3e860ab1a5ebf57834e815
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-03-26 22:14:16 +0000

    Major changes

--- src/datarouter.js
+++ src/datarouter.js
@@ -8,6 +8,9 @@ skyrouterdat.get('/img/:imgsrc', function(req, res){
     res.sendFile(path.join(__dirname, '/img/' + req.params.imgsrc));
 });
 
+skyrouterdat.get('/respack/:ressrc', function(req, res){
+    res.sendFile(path.join(__dirname, '/img/' + req.params.imgsrc));
+});
 
 // Rotas para receber requests API
 module.exports = skyrouterdat;
